32ce321ea641a216bd9738767526d350a7334acf
limaribeiroabraaolimaribeiro-afk <limaribeiroabraaolimaribeiro@gmail.com>
2025-12-31 12:25:44 -0300

    Refactor service worker to use constant cache name

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,41 @@
+const CACHE_NAME = "chiquinho-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./imagens/icone-192.png",
+  "./imagens/icone-512.png"
+];
+
+// INSTALA O APP E SALVA OS ARQUIVOS
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("chiquinho-v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./imagens/"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
+// ATIVA E LIMPA CACHES ANTIGOS
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FUNCIONAMENTO OFFLINE
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
