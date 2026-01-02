741b7da36f7bb1c96ac4eca129e70d4799dc6115
limaribeiroabraaolimaribeiro-afk <limaribeiroabraaolimaribeiro@gmail.com>
2026-01-01 23:14:18 -0300

    Update Firebase configuration with new credentials
    Updated Firebase configuration with new project details.

--- firebase.js
+++ firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 // 🔐 CONFIGURAÇÃO DO SEU FIREBASE
 // 👉 PEGA ISSO NO FIREBASE > Configurações > SDK da Web
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyCy-zUh5M2KiXvWGfXL17onsOtpzGZXf8Y",
+  authDomain: "chiquinho-admin-248b6.firebaseapp.com",
+  projectId: "chiquinho-admin-248b6",
+  storageBucket: "chiquinho-admin-248b6.firebasestorage.app",
+  messagingSenderId: "177897723264",
+  appId: "1:177897723264:web:f59f2f0e0f456c9bbd5d65"
 };
 
 // 🚀 Inicializa Firebase
